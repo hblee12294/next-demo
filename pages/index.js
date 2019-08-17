e84218b71c51ce24c8d846173b58f1fa455422ad
hblee12294 <hblee12294@gmail.com>
2019-08-17 00:56:47 -0700

    build: init repo

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a title="About Page">About</a>
+    </Link>
+    <p>Nice to see u.</p>
+  </div>
+)
+
+export default Index
